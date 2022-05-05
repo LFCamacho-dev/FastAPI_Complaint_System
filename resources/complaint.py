130c8b5c1b517e989a0efc5fc3ea9b2e8214993d
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-05-04 22:59:29 -0500

    added routes to approve and reject complaints, and script to create super user from the command line on startup of the server.

--- resources/complaint.py
+++ resources/complaint.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import APIRouter, Depends
 from starlette.requests import Request
-from managers.auth import oauth2_scheme, is_complainer, is_admin
+from managers.auth import oauth2_scheme, is_complainer, is_admin, is_approver
 from managers.complaint import ComplaintManager
 from schemas.request.complaint import ComplaintIn
 from schemas.response.complaint import ComplaintOut
@@ -29,3 +29,17 @@ async def create_complaints(request: Request, complaint: ComplaintIn):
                status_code=204)
 async def delete_complaint(complaint_id: int):
     await ComplaintManager.delete_complaint(complaint_id)
+
+
+@router.put("/complaints/{complaint_id}/approve",
+            dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
+            status_code=204)
+async def approve_complaint(complaint_id: int):
+    await ComplaintManager.approve(complaint_id)
+
+
+@router.put("/complaints/{complaint_id}/reject",
+            dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
+            status_code=204)
+async def reject_complaint(complaint_id: int):
+    await ComplaintManager.reject(complaint_id)
